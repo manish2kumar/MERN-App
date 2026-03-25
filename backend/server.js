290e8ead7c962cd3a56190f4ce9269fd671825ba
Manish Kumar <157043284+manish2kumar@users.noreply.github.com>
2026-03-25 10:04:13 +0530

    Modify CORS settings to allow all origins
    Updated CORS middleware to allow all origins.

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,13 @@ const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-    'http://localhost:5174',
-    'http://127.0.0.1:5174'
-  ],
+  // origin: [
+  //   'http://localhost:5173',
+  //   'http://127.0.0.1:5173',
+  //   'http://localhost:5174',
+  //   'http://127.0.0.1:5174'
+  // ],
+  origin: true,
   methods: ['GET', 'POST'],
 }));
 app.use(express.json());
